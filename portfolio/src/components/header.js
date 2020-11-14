e3adb37ed3e8471087f74ec8eaa7f04809f3cf4f
Oladunjoye <38365007+Oladunjoye@users.noreply.github.com>
2020-11-14 14:09:57 +0100

    installed plugins

--- portfolio/src/components/header.js
+++ portfolio/src/components/header.js
@@ -5,15 +5,15 @@ import React from "react"
 const Header = ({ siteTitle }) => (
   <header
     style={{
-      background: `rebeccapurple`,
       marginBottom: `1.45rem`,
+      border: `1px solid black`,
     }}
   >
     <div
       style={{
         margin: `0 auto`,
         maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
+        padding: `0.45rem 1.0875rem`,
       }}
     >
       <h1 style={{ margin: 0 }}>
---
+++ portfolio/src/components/hero.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+
+export default function Hero() {
+  return (
+    <div>
+      <h1>Hi, I am Joye Shonubi</h1>
+      <p> I am full stack software engineer</p>
+      <p> You might not know but I really love wildlife</p>
+    </div>
+  )
+}
