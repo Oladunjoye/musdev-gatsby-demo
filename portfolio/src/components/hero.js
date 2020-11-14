236b68642a78d402f87dd0e8e7bb3f64b976d650
Oladunjoye <38365007+Oladunjoye@users.noreply.github.com>
2020-11-14 14:40:54 +0100

    Update hero.js

--- portfolio/src/components/hero.js
+++ portfolio/src/components/hero.js
@@ -1,6 +1,6 @@
 import React from "react"
 import { graphql, useStaticQuery } from "gatsby"
-import styled from "styled-components"
+import styled from "@emotion/styled"
 
 import BackgroundImage from "gatsby-background-image"
 
