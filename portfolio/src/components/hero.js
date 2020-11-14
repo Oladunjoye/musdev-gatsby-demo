2c4fb5ed01f84b27134ae8639491c69372c41ac9
Oladunjoye <38365007+Oladunjoye@users.noreply.github.com>
2020-11-14 14:25:38 +0100

    Update hero.js

--- portfolio/src/components/hero.js
+++ portfolio/src/components/hero.js
@@ -1,12 +1,39 @@
 import React from "react"
+import { graphql, useStaticQuery } from "gatsby"
+import styled from "styled-components"
 
+import BackgroundImage from "gatsby-background-image"
 
 export default function Hero() {
+  const data = useStaticQuery(
+    graphql`
+      query {
+        desktop: file(relativePath: { eq: "seamless-bg-desktop.jpg" }) {
+          childImageSharp {
+            fluid(quality: 90, maxWidth: 1920) {
+              ...GatsbyImageSharpFluid_withWebp
+            }
+          }
+        }
+      }
+    `
+  )
+
+  // Set ImageData.
+  const imageData = data.desktop.childImageSharp.fluid
+
   return (
-    <div>
-      <h1>Hi, I am Joye Shonubi</h1>
-      <p> I am full stack software engineer</p>
-      <p> You might not know but I really love wildlife</p>
-    </div>
+    <BackgroundImage
+      Tag="section"
+      className={className}
+      fluid={imageData}
+      backgroundColor={`#040e18`}
+    >
+      <div>
+        <h1>Hi, I am Joye Shonubi</h1>
+        <p> I am full stack software engineer</p>
+        <p> You might not know but I really love wildlife</p>
+      </div>
+    </BackgroundImage>
   )
 }
